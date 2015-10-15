9f2b8f7a428bef259a080e69cc55d76889131335
danleyb2 <ndieksman@gmail.com>
2015-10-15 12:10:40 +0300

    finish handle button action event

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,20 +2,26 @@ package sample;
 
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent>{
 
 
+    public Button button;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
         primaryStage.setTitle("Hello World");
-        Button button=new Button("Click");
+        button=new Button();
+        button.setText("Click");
+        button.setOnAction(this);
         StackPane layout=new StackPane();
         layout.getChildren().add(button);
         Scene scene=new Scene(layout,400,500);
@@ -26,5 +32,15 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        System.out.println("An event occured");
+        if (event.getSource()==button){
+            //do smt for button
+            System.out.println("A button was clicked");
+        }
+
     }
 }
