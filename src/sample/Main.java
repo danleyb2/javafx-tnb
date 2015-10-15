7e67618ab3f2806dc71c394212c1e7a85e6393c2
danleyb2 <ndieksman@gmail.com>
2015-10-15 13:05:00 +0300

    swich between two scenes using a button

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,29 +4,44 @@ package sample;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
 
     public Button button;
+    Stage window;
+    Scene scene1,scene2;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        primaryStage.setTitle("Hello World");
+        window=primaryStage;
+        window.setTitle("Scene Switch");
+        Label label1=new Label("Scene one");
         button=new Button();
-        button.setText("Click");
+        button.setText("Go to Scene Two");
         button.setOnAction(e -> {
             //handle event
-            System.out.println("Event using lambda expression");
+            window.setScene(scene2);
         });
-        StackPane layout=new StackPane();
-        layout.getChildren().add(button);
-        Scene scene=new Scene(layout,400,500);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        VBox layout1=new VBox(20);
+        layout1.getChildren().addAll(label1,button);
+        scene1=new Scene(layout1,400,500);
+
+        Label label=new Label("Scene Two");
+        Button button2=new Button("Go to scene One");
+        button2.setOnAction(e -> {
+            //handle event
+            window.setScene(scene1);
+        });
+        VBox layout=new VBox();
+        layout.getChildren().addAll(label,button2);
+        scene2=new Scene(layout,400,500);
+        window.setScene(scene1);
+        window.show();
     }
 
 
