d8f7ad12308fcc14569e8bbb1803aa6669edee28
danleyb2 <ndieksman@gmail.com>
2015-10-19 14:40:02 +0300

    optimize imports

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application{
