da09254c48f3957d6cc4a5d94f034254657cd945
danleyb2 <ndieksman@gmail.com>
2015-10-24 19:44:36 +0300

    css styling

--- src/sample/CssStyles.java
+++ src/sample/CssStyles.java
@@ -1,7 +1,36 @@
 package sample;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
  * Created by brian on 10/24/15.
  */
-public class CssStyles {
+public class CssStyles extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("CSS styles themes");
+        VBox vBox=new VBox();
+        HBox hBox=new HBox();
+
+        Label label=new Label("Username");
+        TextField textField=new TextField();
+        hBox.getChildren().addAll(label,textField);
+
+        vBox.getChildren().addAll(hBox);
+
+        Scene scene=new Scene(vBox,400,300);
+        scene.getStylesheets().add("style.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
 }
