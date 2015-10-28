3555891d7217d08c933cc894475ec5ee1239609a
Nyaundi Brian <ndieksman@gmail.com>
2015-10-28 10:12:48 +0300

    IntegerProperties binding

--- src/sample/PropertiesEx.java
+++ src/sample/PropertiesEx.java
@@ -1,18 +1,33 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
- * Created by brian on 10/26/15.
+ * Created by brian on 10/26/15
+ * at 10 : 12 ,
+ * working on JavaFx1.
  */
 public class PropertiesEx extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("String properties");
+        primaryStage.setTitle("String and Integer properties");
+        IntegerProperty x=new SimpleIntegerProperty(5);
+        IntegerProperty y=new SimpleIntegerProperty();
+
+        y.bind(x.multiply(2));
+
+        System.out.println("y: " + y.getValue());
+
+        x.setValue(4);
+
+        System.out.println("y: " + y.getValue());
+
         Product product=new Product();
         product.productCodeProperty().addListener((o, oldV, newV) -> {
             System.out.println("Product code change event... ");
@@ -25,7 +40,7 @@ public class PropertiesEx extends Application {
         StackPane stackPane=new StackPane();
         stackPane.getChildren().add(button);
 
-        primaryStage.setScene(new Scene(stackPane,200,400));
+        primaryStage.setScene(new Scene(stackPane,300,400));
         primaryStage.show();
     }
     public static void main(String[] args){
