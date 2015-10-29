028c85701e7852bb9a2bc08e8ea88116b0bf2c5c
danleyb2 <ndieksman@gmail.com>
2015-10-29 22:32:52 +0300

    create close window method

--- src/fxmlExample/Controller.java
+++ src/fxmlExample/Controller.java
@@ -1,4 +1,9 @@
 package fxmlExample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void closeWindow(ActionEvent actionEvent) {
+        FxmlExample.window.close();
+    }
 }
