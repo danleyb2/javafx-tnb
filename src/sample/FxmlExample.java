c8319254e07756e720ad3e93c32eb9d7b5d6b9a6
danleyb2 <ndieksman@gmail.com>
2015-10-29 21:26:49 +0300

    layout using fxml

--- src/sample/FxmlExample.java
+++ src/sample/FxmlExample.java
@@ -1,9 +1,24 @@
 package sample;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by brian on 10/29/15
  * at 21 : 19 ,
  * working on JavaFx1.
  */
-public class FxmlExample {
+public class FxmlExample extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent parent= FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
 }
